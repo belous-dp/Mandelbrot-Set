e3767d6cbaabef8f38061c0ce270abf88444bb11
Danila Belous <belous.dp@gmail.com>
2023-01-26 10:28:04 +0300

    vs cmake vcpkg build setup

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Danila Belous on 13.01.2023 at 01:21.
+//
+
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QWidget>
+
+int main(int argc, char* argv[]) {
+    QApplication a(argc, argv);
+
+    QWidget w;
+    w.show();
+
+    return a.exec();
+}
