c70fe28b464553429526ead9dfd686e6861bfd8d
Danila Belous <belous.dp@gmail.com>
2023-01-27 12:44:36 +0300

    incremental rendering

--- workers.cpp
+++ workers.cpp
@@ -20,7 +20,8 @@ double color_it(int policy, int it, int max) {
   }
 }
 
-double workers::get_escape_rate(int pos_x, int pos_y, int width, int height, unsigned int cur_img_version) {
+double workers::get_escape_rate(int pos_x, int pos_y, int width, int height, unsigned int cur_img_version,
+                                unsigned num_iterations) {
   double min_x = -2, max_x = 1, min_y = -1.5, max_y = 1.5;
   if (width < (max_x - min_x) * height / (max_y - min_y)) {
     // std::cout << "resize cuz width too small\n";
@@ -40,7 +41,6 @@ double workers::get_escape_rate(int pos_x, int pos_y, int width, int height, uns
 
   double x = 0, y = 0;
 
-  int num_iterations = 1000;
   int iteration = 0;
   while (iteration < num_iterations && x * x + y * y <= 4) {
     if (cur_img_version + 1 < m_max_version.load(std::memory_order_relaxed)) {
@@ -57,29 +57,33 @@ double workers::get_escape_rate(int pos_x, int pos_y, int width, int height, uns
 
 void workers::fill_image(QImage& image) {
   // std::cout << "drawing picture: " << image.width() << 'x' << image.height() << '\n';
-  uchar* data = image.bits();
-  for (int y = 0; y < image.height(); ++y) {
-    uchar* p = data + y * image.bytesPerLine();
-    for (int x = 0; x < image.width(); ++x) {
+  unsigned iter = MIN_ITER;
+  while (iter <= MAX_ITER) {
+    uchar* data = image.bits();
+    for (int y = 0; y < image.height(); ++y) {
+      uchar* p = data + y * image.bytesPerLine();
+      for (int x = 0; x < image.width(); ++x) {
 
-      double escape_rate =
-          get_escape_rate(x, y, image.width(), image.height(), m_cur_version.load(std::memory_order_relaxed));
-      if (escape_rate < -0.5) {
-        return;
+        double escape_rate =
+            get_escape_rate(x, y, image.width(), image.height(), m_cur_version.load(std::memory_order_relaxed), iter);
+        if (escape_rate < -0.5) {
+          return;
+        }
+
+        *p++ = static_cast<uchar>(escape_rate * 0xff);
+        *p++ = static_cast<uchar>(escape_rate * 0.3 * 0xff);
+        *p++ = 0;
       }
-      // std::cout << escape_rate << '\n';
-
-      *p++ = static_cast<uchar>(escape_rate * 0xff);
-      *p++ = static_cast<uchar>(escape_rate * 0.3 * 0xff);
-      *p++ = 0;
     }
+    iter += DELTA;
+    emit image_ready(image);
   }
 }
 
 void workers::render_image(int width, int height) {
   QImage img(width, height, QImage::Format_RGB888);
   assert(m_cur_version <= m_max_version);
-  m_perf_helper.profile([&] { fill_image(img); });
+  fill_image(img);
+  //m_perf_helper.profile([&] { fill_image(img); });
   m_cur_version++;
-  emit image_ready(img);
 }
--- workers.h
+++ workers.h
@@ -21,9 +21,16 @@ public:
   std::atomic<unsigned int> m_max_version{0};
 
 private:
+  constexpr static unsigned MAX_ITER = 2000;
+  constexpr static unsigned MIN_ITER = 15;
+  constexpr static unsigned NSTEPS = 10;
+  constexpr static unsigned DELTA = 200;
+
+
   perf_helper m_perf_helper;
   std::atomic<unsigned int> m_cur_version{0};
 
   void fill_image(QImage& image);
-  double get_escape_rate(int pos_x, int pos_y, int width, int height, unsigned int cur_img_version);
+  double get_escape_rate(int pos_x, int pos_y, int width, int height, unsigned int cur_img_version,
+                         unsigned num_iterations);
 };
